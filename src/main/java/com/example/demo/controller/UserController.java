8417e2e53e06ff17889c2cf64aec7211b8abc52a
hyeon <lhyeonnit@gmail.com>
2024-01-26 15:16:42 +0900

    회원가입 완료

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,21 @@
 package com.example.demo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
+    }
+    @PostMapping("/join")
+    public String join(HttpServletRequest req){
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+
+    }
 }
